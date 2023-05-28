73a3b709e5e79b646b38e68b05d06fc85077aee7
Nathaniel Thickett <thicketn@oregonstate.edu>
2023-05-27 19:27:30 -0700

    copied lab2 code

--- client/ftp_client.py
+++ client/ftp_client.py
@@ -1,2 +1,94 @@
 import os
 os.chdir(‘myfiles’)
+
+#This code was modified by the following people:
+#Austin Cash - cashau
+#Nathaniel Thickett - thicketn
+#Robert Lucas - Lucasrob
+
+
+import socket
+from time import sleep
+from threading import Thread
+import asyncio
+
+IP, DPORT = 'localhost', 8080
+
+# Helper function that converts an integer into a string of 8 hexadecimal digits
+# Assumption: integer fits in 8 hexadecimal digits
+def to_hex(number):
+    # Verify our assumption: error is printed and program exists if assumption is violated
+    assert number <= 0xffffffff, "Number too large"
+    return "{:08x}".format(number)
+
+##################################
+# TODO: Implement me for Part 1! #
+##################################
+async def recv_intro_message(reader: asyncio.StreamReader):
+    
+    full_data = await reader.readline()
+    return full_data.decode()
+    
+
+
+##################################
+# TODO: Implement me for Part 2! #
+##################################
+async def send_long_message(writer: asyncio.StreamWriter, data):
+    # TODO: Send the length of the message: this should be 8 total hexadecimal digits
+    #       This means that ffffffff hex -> 4294967295 dec
+    #       is the maximum message length that we can send with this method!
+    #       hint: you may use the helper function `to_hex`. Don't forget to encode before sending!
+
+    # Add a delay to simulate network latency
+    await asyncio.sleep(1)
+
+    writer.write(to_hex(len(data)).encode())
+    writer.write(data.encode())
+
+    await writer.drain()
+
+
+async def connect(i):
+    reader, writer = await asyncio.open_connection(IP, DPORT)
+
+    intro = await recv_intro_message(reader)
+    print(intro)
+
+    long_msg = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX" * 100 + f"  CLIENT: " + str(i)
+
+    await send_long_message(writer, long_msg)
+
+    print("Done sending", i)
+
+
+    return 0
+
+
+def inputNumber(message):
+  while True:
+    try:
+       userInput = int(input(message))       
+    except ValueError:
+       print("Not an integer! Try again.")
+       continue
+    else:
+       return userInput 
+       break
+   
+
+async def main():
+    tasks = []
+    
+    x = inputNumber("Enter number of connections to open: ")
+    
+    for i in range(x):
+        tasks.append(connect(i))
+
+    await asyncio.gather(*tasks)
+    print("done")
+
+# Run the `main()` function
+if __name__ == "__main__":
+    asyncio.run(main())
+
--- server/ftp_server.py
+++ server/ftp_server.py
@@ -1,2 +1,80 @@
 import os
 os.chdir(‘myfiles’)
+
+#This code was modified by the following people:
+#Austin Cash - cashau
+#Nathaniel Thickett - thicketn
+#Robert Lucas - Lucasrob
+
+
+import socket
+import asyncio
+
+INTERFACE, SPORT = 'localhost', 8080
+CHUNK = 100
+
+
+##################################
+# TODO: Implement me for Part 1! #
+##################################
+async def send_intro_message(writer):
+    # TODO: Replace {ONID} with your ONID (mine is lyakhovs)
+    #       and {MAJOR} with your major (i.e. CS, ECE, any others?)
+    intro_message = "Hello! Welcome to our (thicketn - CS, cashau - CS, Lucasrob - ECE) server!\n"
+
+    # TODO: Send this intro message to the client. Don't forget to encode() it!
+    #       hint: use the `conn` handle and `sendall`!
+    writer.write(intro_message.encode())
+    await writer.drain()
+
+
+##################################
+# TODO: Implement me for Part 2! #
+##################################
+async def receive_long_message(reader: asyncio.StreamReader):
+    # First we receive the length of the message: this should be 8 total hexadecimal digits!
+    # Note: `socket.MSG_WAITALL` is just to make sure the data is received in this case.
+    data_length_hex = await reader.readexactly(8)
+
+    # Then we convert it from hex to integer format that we can work with
+    data_length = int(data_length_hex, 16)
+
+   
+    full_data = await reader.readexactly(data_length)
+    return full_data.decode()
+    
+   
+
+async def handle_client(reader, writer):
+    """
+    Part 1: Introduction
+    """
+    # TODO: send the introduction message by implementing `send_intro_message` above.
+    await send_intro_message(writer)
+
+    """
+    Part 2: Long Message Exchange Protocol
+    """
+    # TODO: Implement function above
+    message = await receive_long_message(reader)
+
+    # I'm only printing the last 8 characters of the message here because it's long
+    print("done: " +  message[-11:])
+
+    writer.close()
+    await writer.wait_closed()
+
+
+async def main():
+
+    server = await asyncio.start_server(
+            handle_client,
+            INTERFACE, SPORT
+    )
+
+    async with server:
+        await server.serve_forever()
+
+# Run the `main()` function
+if __name__ == "__main__":
+    asyncio.run(main())
